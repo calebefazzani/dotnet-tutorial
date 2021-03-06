545a3a883e1777a21f92f38d59df1745e7e4e8ea
calebe <calebe.fazzani.facul@gmail.com>
2021-03-06 11:09:48 -0300

    Primeiro tutorial EF

--- entityFrameworkTutorial/EFGetStarted/Program.cs
+++ entityFrameworkTutorial/EFGetStarted/Program.cs
@@ -2,11 +2,39 @@
 
 namespace EFGetStarted
 {
+    using System.Linq;
+    using Models;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            using (var db = new BloggingContext())
+            {
+                // Criação
+                Console.WriteLine("Adicionando novo blog");
+                db.Add(new Blog {Url = "http://blogs.msdn.com/adonet"});
+                db.SaveChanges();
+                
+                // Busca
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Atualização
+                Console.WriteLine("Atualizando");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post {Title = "Hello World", Content = "Yay"});
+
+                db.SaveChanges();
+                
+                Console.WriteLine("Deletando");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
         }
     }
 }
